c722d63cf1a196d967cdbd9b8969ea5270861eae
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2016-07-13 13:22:43 +0400

    basic server up, basic html, time to add mongodb

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 	var app = express();
 
 	app.get('/', function (req, res) {
-		res.sendFile(__dirname + '/index.html');
+		res.sendFile(__dirname + '/public/index.html');
 	});
 
 	app.listen(8080);
