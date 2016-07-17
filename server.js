9f0aa353355f7fed5c11e5c96f519ee38047a018
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2016-07-17 11:01:00 +0400

    a few changes, installed body-parser

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 (function (){
 	"use strict";
 
+	var port = process.env.PORT || 8080;
+
 	var express = require("express");
-
 	var app = express();
+	var router = express.Router();
 
-	app.get('/', function (req, res) {
-		res.sendFile(__dirname + '/public/index.html');
+	app.use(express.static('public'));
+
+	app.get('/test', function (req, res) {
+		console.log("in test");
+
+		res.sendFile(__dirname + '/public/test.html');
 	});
 
-	app.listen(8080);
+	app.use('/test', router);
+
+	app.listen(port);
 
 	console.log("server started");
 
