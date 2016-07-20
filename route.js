60090abc7383e29add1191b287ad5f17b10627bb
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2016-07-20 11:04:04 +0400

    added updateFile, basic read from one file

--- route.js
+++ route.js
@@ -1,10 +1,12 @@
 (function () {
 	"use strict";
 
-
+	var fs = require("fs");
 	var db = require("./db.js");
 	var express = require("express");
 	var router = express.Router();
+
+	var path = "C:\\Users\\user\\Documents\\Etisalat Projects\\Etisalat Bitstream\\etisalat_bitstream\\test\\";
 
 	// template
 	// router.route('/pg')
@@ -27,8 +29,9 @@
 			// res.json("should return all devices");
 
 			db.get().query('SELECT * FROM test_crud', function(err, rows, fields) {
-			  if (err) throw err;
-			  res.json("data: " + JSON.stringify(rows));
+				if (err) throw err;
+
+				res.json(rows);
 			});
 
 		});
@@ -36,6 +39,23 @@
 	router.route('/viewCritical')
 		.get(function (req, res) {
 			res.json("should return critical devices only");
+		});
+
+	router.route('/updateFile')
+		.get(function (req, res) {
+			
+			fs.readdir(path, function(err, files) {
+			    //res.json(files);
+
+			    fs.readFile(path + files[0], "utf-8", function (err, data) {
+				   if (err) {
+				       return console.error(err);
+				   }
+				   // data = data.replace(/[^\w\s]/gi, '');
+				   res.send(data);
+				});
+			});
+
 		});
 
 	module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 
 	var db = require("./db.js");
 	var async = require('async');
-	// var data = require('./test/data.json');
 
 	var express = require("express");
 	var app = express();
@@ -19,26 +18,8 @@
 
 	app.use('/', router);
 
-	var data = {
-		tables: {
-			test_crud: [
-			 {id: 1, test_num: 33, test_string: "a string"},
-			 {id: 2, test_num: 5, test_string: "asdf"},
-			]
-		},
-	};
-
 	// Connect to Server and MySQL on start
 	db.connect(db.MODE_TEST, function(err) {
-		// db.drop(Object.keys(data.tables), function(err) {
-		// 	if (err) return console.log(err);
-		// 	console.log('Tables have been dropped...');
-		// });
-
-		// db.fixtures(data, function(err) {
-		// 	if (err) return console.log(err);
-		// 	console.log('Data has been loaded...');
-		// });
 		if (err) {
 		console.log('Unable to connect to MySQL.');
 		process.exit(1);
